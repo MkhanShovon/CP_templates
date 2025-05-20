d09ade7341e0f2e37752b764b05188ab5d5a4c5d
MKhanShovon <mkhanshovon@gmail.com>
2025-05-20 20:04:55 +0600

    ET added

---
+++ Euler's_totient.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int phi(int n) {//O(sqrt(n))
+    int result = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+
+void phi_1_to_n(int n) { //O(n log log n)
+    vector<int> phi(n + 1);
+    for (int i = 0; i <= n; i++)
+        phi[i] = i;
+
+    for (int i = 2; i <= n; i++) {
+        if (phi[i] == i) {
+            for (int j = i; j <= n; j += i)
+                phi[j] -= phi[j] / i;
+        }
+    }
+}
+
+
+void phi_1_to_n(int n) { //O(nlogn) => using Divisor sum property
+    vector<int> phi(n + 1);
+    phi[0] = 0;
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++)
+        phi[i] = i - 1;
+
+    for (int i = 2; i <= n; i++)
+        for (int j = 2 * i; j <= n; j += i)
+              phi[j] -= phi[i];
+}
+
