ca11bffb0586963359fa15099ea11d54e8855387
Monjur Hossain Khan Shovon <125968931+MkhanShovon@users.noreply.github.com>
2025-11-15 00:49:45 +0600

    Add negative cycle detection
    Implement Floyd-Warshall algorithm to detect negative cycles.

---
+++ Negative_cycle.cpp
@@ -0,0 +1,56 @@
+//Bellman_Ford
+struct Edge {
+    int a, b, cost;
+};
+
+int n;
+vector<Edge> edges;
+const int INF = 1000000000;
+
+void solve() {
+    vector<int> d(n, 0);
+    vector<int> p(n, -1);
+    int x;
+
+    for (int i = 0; i < n; ++i) {
+        x = -1;
+        for (Edge e : edges) {
+            if (d[e.a] + e.cost < d[e.b]) {
+                d[e.b] = max(-INF, d[e.a] + e.cost);
+                p[e.b] = e.a;
+                x = e.b;
+            }
+        }
+    }
+
+    if (x == -1) {
+        cout << "No negative cycle found.";
+    } else {
+        for (int i = 0; i < n; ++i)
+            x = p[x];
+
+        vector<int> cycle;
+        for (int v = x;; v = p[v]) {
+            cycle.push_back(v);
+            if (v == x && cycle.size() > 1)
+                break;
+        }
+        reverse(cycle.begin(), cycle.end());
+
+        cout << "Negative cycle: ";
+        for (int v : cycle)
+            cout << v << ' ';
+        cout << endl;
+    }
+}
+
+//Floyd warshalL
+//Initially  d[v][v] = 0
+for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+        for (int t = 0; t < n; ++t) {
+            if (d[i][t] < INF && d[t][t] < 0 && d[t][j] < INF)
+                d[i][j] = - INF; 
+        }
+    }
+}
