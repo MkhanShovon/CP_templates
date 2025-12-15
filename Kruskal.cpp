576a74aaa12830030c3c088a6905bbbe779a355a
Monjur Hossain Khan Shovon <125968931+MkhanShovon@users.noreply.github.com>
2025-12-15 14:20:55 +0600

    Add implementation of Kruskal's algorithm

---
+++ Kruskal.cpp
@@ -0,0 +1,279 @@
+#include <iostream>
+#include <bits/stdc++.h>
+// #include <sys/resource.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace chrono;
+using namespace __gnu_pbds;
+template <typename T>
+using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T>
+using omset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename R>
+using o_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// Pragmas
+#pragma GCC optimize("O3,unroll-loops")
+// #ifndef LOCAL_PROJECT
+// #pragma GCC target("avx2")
+// #endif
+// #pragma GCC optimize("Ofast")
+
+// Speed
+inline void FastIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+// Aliases
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+using i128 = __int128_t;
+
+
+// Constants
+const lld pi = 3.141592653589793238;
+const ll INF = 1e16;
+
+// TypeDEf
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef unordered_map<ll, ll> umll;
+typedef map<ll, ll> mll;
+typedef priority_queue<int, vector<int>, greater<int>> minHeap;
+
+// Macros
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fl(i, n) for (ll i = 0; i < n; i++)
+#define fls(i, m, n) for (ll i = m; i <= n; i++)
+#define rl(i, m, n) for (ll i = n; i >= m; i--)
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pn cout << "NO\n";
+#define vr(v) v.begin(), v.end()
+#define rv(v) v.rbegin(), v.rend()
+#define edl '\n'
+#define len(v) v.size()
+#define nl cout << edl;
+
+// Debug
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '"' << x << '"'; }
+void __print(const string &x) { cerr << '"' << x << '"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+// Operator overloads
+template <typename T1, typename T2> // cin >> pair<T1, T2>
+istream &operator>>(istream &istream, pair<T1, T2> &p)
+{
+    return (istream >> p.first >> p.second);
+}
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T1, typename T2> // cout << pair<T1, T2>
+ostream &operator<<(ostream &ostream, const pair<T1, T2> &p)
+{
+    return (ostream << p.first << " " << p.second);
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c)
+{
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+// Utility functions
+#define print(x) cout << x << '\n'
+
+template <typename T>
+ll sumvec(vector<T> v)
+{
+    ll n = v.size();
+    ll s = 0;
+    fl(i, n) s += v[i];
+    return s;
+}
+
+long long rng()
+{
+    static std::mt19937 gen(
+        std::chrono::steady_clock::now().time_since_epoch().count());
+    return std::uniform_int_distribution<long long>(0, INT64_MAX)(gen);
+}
+
+void preSolve()
+{
+    // rng = mt19937(chrono::steady_clock::now().time_since_epoch().count());
+    // allocateStackMax();
+}
+
+class DSU {
+private:
+    vector<ll> parent, size, minElement, maxElement;
+    ll n;
+    
+public:
+    DSU(ll nn) {
+        this->n = nn+1;
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);  // Each element is its own set initially, so size is 1
+        minElement.resize(n + 1);
+        maxElement.resize(n + 1);
+        
+        for (ll i = 1; i <= n; ++i) {
+            parent[i] = i;
+            minElement[i] = i;
+            maxElement[i] = i;
+        }
+    }
+
+    ll find(ll u) {
+        if (parent[u] != u) {
+            parent[u] = find(parent[u]); // Path Compression
+        }
+        return parent[u];
+    }
+
+    void unite(ll u, ll v) {
+        ll rootU = find(u);
+        ll rootV = find(v);
+        
+        if (rootU != rootV) {
+            // Union by Size
+            if (size[rootU] < size[rootV]) {
+                swap(rootU, rootV);  // Ensure rootU is always the larger set
+            }
+
+            parent[rootV] = rootU;  // Merge the smaller set into the larger set
+            size[rootU] += size[rootV];
+            minElement[rootU] = min(minElement[rootU], minElement[rootV]);
+            maxElement[rootU] = max(maxElement[rootU], maxElement[rootV]);
+        }
+    }
+
+    tuple<int, int, int> get(int v) {
+        int rootV = find(v);
+        return {minElement[rootV], maxElement[rootV], size[rootV]};
+    }
+};
+
+// Code
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    vector<tuple<ll, ll, ll>> e;
+    fl(i, m)
+    {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        e.pb({w, u, v});
+    }
+
+    DSU dsu(n + 1);
+    sort(vr(e));
+    ll ans = 0;
+    for(auto [w, u, v] : e)
+    {
+        if(dsu.find(u) != dsu.find(v))
+        {
+            ans += w;
+            // debug(u, v, w);
+            dsu.unite(u, v);
+        }
+    }
+    fls(i, 2, n)
+    {
+        if(dsu.find(1) != dsu.find(i))
+        {
+            print("IMPOSSIBLE");
+            return;
+        }
+    }
+    print(ans);
+}
+// Main
+signed main()
+{
+    // #ifndef ONLINE_JUDGE
+    //     freopen("Error.txt", "w", stderr);
+    // #endif
+    FastIO();
+    preSolve();
+    ll t = 1;
+    // cin >> t;
+    fl(i, t)
+    {
+        solve();
+    }
+    // solve();
+    // fl(i,t) //Kickstart
+    // {
+    //     cout<<"Case #"<<i+1<<": ";
+    //     solve();
+    //     cout<<"\n";
+    // }
+    return 0;
+}
+// End
