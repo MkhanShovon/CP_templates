cadd5e55f3ab064ca0739f229cc590b0f3a68119
Monjur Hossain Khan Shovon <125968931+MkhanShovon@users.noreply.github.com>
2026-01-09 19:32:28 +0600

    Optimized sieve added

--- sieve.cpp
+++ sieve.cpp
@@ -1,3 +1,32 @@
+//Optimized sieve
+vector<long long int> prime;    //List of primes
+const int nx = 1e6 + 10;        // range of prime numbers
+
+void generate_prime()           //For generating all primes within 10^6 using sieve of eratosthenes
+{
+
+    bool flag[nx]={};
+
+    int rt = 1e3;               // because root of 10^6 is 10^3
+
+    for(int i=3; i<rt; i+=2)
+    {
+        if(!flag[i])
+        {
+            for(int j=i*i; j<nx; j+=(i<<1))
+            {
+                flag[j] = true;
+            }
+        }
+    }
+
+    prime.push_back(2);
+    for(int i=3; i<nx; i+=2)
+    {
+        if(!flag[i]) prime.push_back(i);
+    }
+}
+
 const ll N = 2e5;
 vll prime;
 void sieve()
